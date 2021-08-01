f286c7135506a6e87cdd011d12e62db61e1e0b0c
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-08-01 14:44:27 +0300

    feat: realize closeButton

---
+++ RSSchool_T9/RSSchool_T9/Views/CloseButton.swift
@@ -0,0 +1,61 @@
+//
+// 📰 🐸 
+// Project: RSSchool_T9
+// 
+// Author: Evgeniy Petlitskiy
+// On: 1.08.21
+// 
+// Copyright © 2021 RSSchool. All rights reserved.
+
+import UIKit
+
+class CloseButton: UIButton {
+    
+    private lazy var xImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: "xmark")
+        imageView.tintColor = .black
+        imageView.isUserInteractionEnabled = false
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.black.cgColor
+        layer.cornerRadius = 20
+        
+        self.addSubview(xImageView)
+        
+        NSLayoutConstraint.activate([
+            
+            self.heightAnchor.constraint(equalToConstant: 40),
+            self.widthAnchor.constraint(equalToConstant: 40),
+            
+            xImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            xImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            xImageView.heightAnchor.constraint(equalToConstant: 21),
+            xImageView.widthAnchor.constraint(equalToConstant: 19)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            UIView.transition(with: self, duration: 0.1, options: [.transitionCrossDissolve]) { [self] in
+                if isHighlighted {
+                    xImageView.tintColor = UIColor(red: 0.475, green: 0.475, blue: 0.475, alpha: 1)
+                } else {
+                    xImageView.tintColor = .black
+                }
+            }
+        }
+    }
+}
