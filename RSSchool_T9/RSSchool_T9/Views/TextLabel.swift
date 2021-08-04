7e0063d08cfeb2a3fcf4c1358ac1364ec5641223
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-08-04 22:19:53 +0300

    fix(TextLabel): bug when text is truncated

--- RSSchool_T9/RSSchool_T9/Views/TextLabel.swift
+++ RSSchool_T9/RSSchool_T9/Views/TextLabel.swift
@@ -9,31 +9,44 @@
 
 import UIKit
 
-class TextLabel: UILabel {
+class TextLabel: UIView {
+    
+    lazy var textLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.black
+        label.font = UIFont(name: "Rockwell-Regular", size: 24)
+        label.textColor = .white
+        label.numberOfLines = 0
+        return label
+    }()
 
     init(with text: String?) {
         super.init(frame: CGRect.zero)
-        
         backgroundColor = .black
         
-        self.text = text
-        font = UIFont(name: "Rockwell-Regular", size: 24)
-        textColor = .white
-        
-        numberOfLines = 0
-        
+        self.addSubview(textLabel)
+        textLabel.text = text
+
         layer.cornerRadius = 8
         layer.borderWidth = 1
         layer.borderColor = UIColor.white.cgColor
+        
+        NSLayoutConstraint.activate([
+            textLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30),
+            textLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 30),
+            textLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+            textLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0),
+        ])
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func drawText(in rect: CGRect) {
-        let insets = UIEdgeInsets(top: 30, left: 30, bottom: 30, right: 40)
-        super.drawText(in: rect.inset(by: insets))
-    }
+//    override func drawText(in rect: CGRect) {
+//        let insets = UIEdgeInsets(top: 30, left: 30, bottom: 30, right: 40)
+//        super.drawText(in: rect.inset(by: insets))
+//    }
 
 }
